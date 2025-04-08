5fd88c25efbf99c4cc43e6eded9c868fbd671ba1
Gonçalo <up202106066@up.pt>
2025-04-08 11:01:40 +0100

    mensagem do commit

---
+++ Scada/Config/Table.ml
@@ -0,0 +1,57 @@
+§
+Language0§Language1
+Conveyor1§Conveyor1
+Conveyor2§Conveyor2
+Conveyor3§Conveyor3
+conveyor1§conveyor1
+conveyor2§conveyor2
+conveyor3§conveyor3
+sensor1§sensor1
+sensor2§sensor2
+STOP 1§STOP 1
+Conveyor 1 overloaded!§Conveyor 1 overloaded!
+Conveyor 2 Overload!§Conveyor 2 Overload!
+Alarms§Alarms
+Active§Active
+Ack§Ack
+Message§Message
+Date§Date
+Time§Time
+Class 1§Class 1
+Class 2§Class 2
+To ack§To ack
+Excluded§Excluded
+Alarm History§Alarm History
+Start date§Start date
+Start time§Start time
+End date§End date
+End time§End time
+Conveyor 1 switched on.§Conveyor 1 switched on.
+Conveyor 2 switched on.§Conveyor 2 switched on.
+Events§Events
+Close§Close
+close§close
+Button§Button
+Blink ON§Blink ON
+Blink OFF§Blink OFF
+Automatic§Automatic
+Manual§Manual
+Maintenance§Maintenance
+C1§C1
+T1§T1
+T2§T2
+T3§T3
+Cell 1§Cell 1
+T4§T4
+Finish§Finish
+On§On
+Choose Tool§Choose Tool
+Operate§Operate
+Off§Off
+Data - hora§Data - hora
+Label§Label
+kgfvjh§kgfvjh
+Mantenence§Mantenence
+-> T1§-> T1
+Wating §Wating 
+Changing Tool§Changing Tool
