477ed6452cc3f0e752eb5d40721416b55e319a65
Luis Manuel <luis.m.sousa10@gmail.com>
2025-05-29 22:04:07 +0100

    ProdLIne python class

--- MES_ERP.py
+++ MES_ERP.py
@@ -6,6 +6,7 @@ import os
 from dataclasses import dataclass, field
 from typing import List
 from collections import deque
+from datetime import datetime, timedelta
 
 # --- Definition of Pieces and Tools ---
 
@@ -54,6 +55,145 @@ def simulate_transformation_path(p: Pieces):
         current = result
     return current
 
+class ProdLine:
+    def __init__(this, entry_node, trans_m, trans_t, cell_free, cell_steps, m1t, m2t, cell_num):
+        this.entry_node = entry_node
+        this.trans_m_node = trans_m
+        this.trans_t_node = trans_t
+        this.cell_free_node = cell_free
+        this.cell_steps_node = cell_steps
+        this.m1t = m1t
+        this.m2t = m2t
+        this.cell_num = cell_num
+        this.busy = False
+        this.wait = datetime.now()
+        this.steps = 0
+        this.state = 0
+        this.piece = 0
+    
+    def tick(this):
+        if(datetime.now() > this.wait and this.busy):
+
+            
+            if(this.state == 0):
+                # Espera até poder processar
+                cell_free = this.cell_free_node.get_value()
+                if isinstance(cell_free, (list, tuple)) and cell_free[this.cell_num] == 0:
+                    print("Cell num",this.cell_num,"State 0 C1 is busy processing...")
+                    this.state+=1
+                    this.wait = datetime.now() + timedelta(seconds=0.5)
+
+            elif(this.state==1):
+                # Espera que a peça entre no tapete
+                cell_free = this.cell_free_node.get_value()
+                if isinstance(cell_free, (list, tuple)) and cell_free[this.cell_num] == 0:
+                    print("Cell num",this.cell_num,"State 1 C1 is busy processing...")
+                    this.state+=1
+                this.wait = datetime.now() + timedelta(seconds=0.5)
+
+            elif(this.state==2):
+                #Espera que a peça de facto saia do tapete
+                cell_free = this.cell_free_node.get_value()
+                if isinstance(cell_free, (list, tuple)) and cell_free[this.cell_num] == 1:
+                    this.busy = False
+                    print("Done")
+                    return simulate_transformation_path(this.piece) if this.piece != 0 else None
+                else:
+                    this.wait = datetime.now() + timedelta(seconds=0.5)
+
+            '''
+            elif(this.state == 1):
+                # Coloca a peça no início
+                cell_steps = this.cell_steps_node.get_value()
+                if not isinstance(cell_steps, (list, tuple)):
+                    print("Error reading Cell_steps array")
+                    cell_steps = [0]*21  # fallback
+
+                # Atualizar passo do C1 para o número de passos da peça
+                cell_steps = list(cell_steps)  # garantir mutável
+                cell_steps[this.cell_num] = len(this.piece.TRANSFORM) if this.piece != 0 else 0  # evita erro se piece for 0
+
+                # Escrever vetor atualizado para PLC
+                this.cell_steps_node.set_value(ua.Variant(cell_steps, ua.VariantType.Int16))
+
+                # Enviar entrada e transformações para C1, apenas se piece for diferente de 0
+                if this.piece != None:
+                    entry_list = this.entry_node.get_value()
+                    entry_list[this.cell_num] = this.piece.Initial_Piece
+                    this.entry_node.set_value(ua.Variant(entry_list, ua.VariantType.Int16))
+                    print("P2: TROQUEI UMA VEZ")
+
+                    MAX_TRANS = 6
+                    tools = this.piece.TRANSFORM[:MAX_TRANS]
+                    times = this.piece.TIMES[:MAX_TRANS]
+
+                    while len(tools) < MAX_TRANS:
+                        tools.append(0)
+                    while len(times) < MAX_TRANS:
+                        times.append(0)
+
+                    times_ms = [t * 1000 for t in times]
+
+                    this.trans_m_node.set_value(ua.Variant(tools, ua.VariantType.Int16))
+                    print("P3: TIMES", times_ms)
+                    this.trans_t_node.set_value(ua.Variant(times_ms, ua.VariantType.Int64))
+
+                    print(f"P4: Sent recipe: P{order_type} → initial {this.piece.Initial_Piece}, {len(this.piece.TRANSFORM)} steps")
+                else:
+                    print("No active piece to send recipe for (piece = 0)")
+                this.state+=1
+                '''
+
+    def start(this, piece):
+        if(not this.busy):
+            cell_steps = this.cell_steps_node.get_value()
+            if not isinstance(cell_steps, (list, tuple)):
+                print("Error reading Cell_steps array")
+                cell_steps = [0]*21  # fallback
+
+            # Atualizar passo do C1 para o número de passos da peça
+            cell_steps = list(cell_steps)  # garantir mutável
+            cell_steps[this.cell_num] = len(piece.TRANSFORM) if piece != 0 else 0  # evita erro se piece for 0
+
+            # Escrever vetor atualizado para PLC
+            this.cell_steps_node.set_value(ua.Variant(cell_steps, ua.VariantType.Int16))
+
+            # Enviar entrada e transformações para C1, apenas se piece for diferente de 0
+            if piece != 0:
+                entry_list = this.entry_node.get_value()
+                entry_list[this.cell_num] = piece.Initial_Piece
+                this.entry_node.set_value(ua.Variant(entry_list, ua.VariantType.Int16))
+
+                MAX_TRANS = 6
+                tools = piece.TRANSFORM[:MAX_TRANS]
+                times = piece.TIMES[:MAX_TRANS]
+
+                while len(tools) < MAX_TRANS:
+                    tools.append(0)
+                while len(times) < MAX_TRANS:
+                    times.append(0)
+
+                times_ms = [t * 1000 for t in times]
+
+                this.trans_m_node.set_value(ua.Variant(tools, ua.VariantType.Int16))
+                print("TIMES", times_ms)
+                this.trans_t_node.set_value(ua.Variant(times_ms, ua.VariantType.Int64))
+
+                print(f"Sent recipe: P{order_type} → initial {piece.Initial_Piece}, {len(piece.TRANSFORM)} steps")
+                this.piece = piece
+
+                this.state = 0
+                this.busy = True
+                return True
+            else:
+                print("No active piece to send recipe for (piece = 0)")
+
+            ##entry_node.set_value(ua.Variant(0, ua.VariantType.Int16))
+        return False
+
+
+
+
 # --- Load orders from ERP ---
 order_queue = deque()
 orders_folder = os.path.join(os.path.dirname(__file__), 'Orders')
@@ -77,8 +217,10 @@ def read_codesys_variables():
         print(f"Connected to OPC UA Server at {server_url}")
 
         entry_node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.Entry_pieces")
-        trans_m_node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.C1_transformations_M")
-        trans_t_node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.C1_transformations_T")
+        trans_m_node_1 = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.C1_transformations_M")
+        trans_t_node_1 = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.C1_transformations_T")
+        trans_m_node_2 = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.C2_transformations_M")
+        trans_t_node_2 = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.C2_transformations_T")
         cell_free_node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.Cell_free")
         cell_steps_node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.Cell_steps")
 
@@ -90,6 +232,9 @@ def read_codesys_variables():
 
         WH1 = [0] * 32
         WH2 = [0] * 32
+
+        line1 = ProdLine(entry_node, trans_m_node_1, trans_t_node_1, cell_free_node, cell_steps_node, [1,2,3], [2,3,4], 4)
+        line2 = ProdLine(entry_node, trans_m_node_2, trans_t_node_2, cell_free_node, cell_steps_node, [2,3,4], [3,4,5], 5)
 
         while len(order_queue) > 0:
             order_type, quantity = order_queue.popleft()
@@ -106,7 +251,7 @@ def read_codesys_variables():
                 if isinstance(cell_free, (list, tuple)):
                     if cell_free[CELL_CA] == 1:
                         entry_list = entry_node.get_value()
-                        entry_list[1] = piece.Initial_Piece
+                        entry_list[0] = piece.Initial_Piece
                         entry_node.set_value(ua.Variant(entry_list, ua.VariantType.Int16))
                         print(f"Sent initial piece {piece.Initial_Piece} to CA_entry_piece (CA is free)")
                     else:
@@ -147,78 +292,37 @@ def read_codesys_variables():
                     print(f"Waiting for piece {piece.Initial_Piece} to be available in WH1...")
                 time.sleep(0.5)
 
-            # Ler o vetor Cell_steps inteiro (ou ler o valor atual em CELL_C1)
-            cell_steps = cell_steps_node.get_value()
-            if not isinstance(cell_steps, (list, tuple)):
-                print("Error reading Cell_steps array")
-                cell_steps = [0]*21  # fallback
+            while not line2.start(piece):
+                pass
 
-            # Atualizar passo do C1 para o número de passos da peça
-            cell_steps = list(cell_steps)  # garantir mutável
-            cell_steps[CELL_C1] = len(piece.TRANSFORM) if piece != 0 else 0  # evita erro se piece for 0
-
-            # Escrever vetor atualizado para PLC
-            cell_steps_node.set_value(ua.Variant(cell_steps, ua.VariantType.Int16))
-
-            # Enviar entrada e transformações para C1, apenas se piece for diferente de 0
-            if piece != 0:
-                entry_list = entry_node.get_value()
-                entry_list[4] = piece.Initial_Piece
-                entry_node.set_value(ua.Variant(entry_list, ua.VariantType.Int16))
-
-                MAX_TRANS = 6
-                tools = piece.TRANSFORM[:MAX_TRANS]
-                times = piece.TIMES[:MAX_TRANS]
-
-                while len(tools) < MAX_TRANS:
-                    tools.append(0)
-                while len(times) < MAX_TRANS:
-                    times.append(0)
-
-                times_ms = [t * 1000 for t in times]
-
-                trans_m_node.set_value(ua.Variant(tools, ua.VariantType.Int16))
-                print("TIMES", times_ms)
-                trans_t_node.set_value(ua.Variant(times_ms, ua.VariantType.Int64))
-
-                print(f"Sent recipe: P{order_type} → initial {piece.Initial_Piece}, {len(piece.TRANSFORM)} steps")
-            else:
-                print("No active piece to send recipe for (piece = 0)")
-
-            # Esperar C1 processar (0 ocupado, depois 1 livre)
-            while True:
-                cell_free = cell_free_node.get_value()
-                if isinstance(cell_free, (list, tuple)) and cell_free[CELL_C1] == 0:
-                    print("C1 is busy processing...")
-                    break
-                time.sleep(0.5)
+            result_piece = None
+            while(line2.busy):
+                result_piece = line2.tick()
+            print(result_piece)
+            
 
             ##entry_node.set_value(ua.Variant(0, ua.VariantType.Int16))
 
-            while True:
-                cell_free = cell_free_node.get_value()
-                if isinstance(cell_free, (list, tuple)) and cell_free[CELL_C1] == 1:
-                    result_piece = simulate_transformation_path(piece) if piece != 0 else None
-                    if result_piece is None:
-                        print("Error: invalid transformation path for piece or no piece to process")
-                    else:
-                        try:
-                            index = WH2.index(0)
-                            WH2[index] = result_piece
-                            print(f"Stored transformed piece {result_piece} in WH2 at position {index}")
 
-                            # Extrair peça final encomendada para CX_entry_piece
-                            if result_piece == order_type:
-                                entry_list = entry_node.get_value()
-                                entry_list[11] = result_piece
-                                entry_node.set_value(ua.Variant(entry_list, ua.VariantType.Int16))
-                                print(f"Extracted final piece {result_piece} from WH2 to CX_entry_piece")
-                                WH2[index] = 0
+            if result_piece is None:
+                print("Error: invalid transformation path for piece or no piece to process")
+            else:
+                try:
+                    index = WH2.index(0)
+                    WH2[index] = result_piece
+                    print(f"Stored transformed piece {result_piece} in WH2 at position {index}")
 
-                        except ValueError:
-                            print("Warning: WH2 is full, cannot store more pieces")
-                    break
-                time.sleep(0.5)
+                    # Extrair peça final encomendada para CX_entry_piece
+                    if result_piece == order_type:
+                        entry_list = entry_node.get_value()
+                        entry_list[11] = result_piece
+                        entry_node.set_value(ua.Variant(entry_list, ua.VariantType.Int16))
+                        print(f"Extracted final piece {result_piece} from WH2 to CX_entry_piece")
+                        WH2[index] = 0
+
+                except ValueError:
+                    print("Warning: WH2 is full, cannot store more pieces")
+
 
                 
 
